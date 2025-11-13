bc7f48d7de92ae49d34ee05f9674d87173b960fc
cmakhmud99 <mahmud.cebrayilov.2016@mail.ru>
2025-11-13 16:26:58 +0400

    Add shop views and templates

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from django.shortcuts import redirect
 from . import views
 from django.contrib.auth import views as auth_views
 
 app_name = 'shop'
 
 urlpatterns = [
+    # Root redirect to login
+    path('', lambda request: redirect('shop:login'), name='root_redirect'),
+    
     path('worker/', views.worker_dashboard, name='worker'),
     path('api/scan/', views.scan_barcode, name='scan_barcode'),
     path('api/sale/', views.process_sale, name='process_sale'),
